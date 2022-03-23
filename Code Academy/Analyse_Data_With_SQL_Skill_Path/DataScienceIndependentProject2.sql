198c573d50e5ce03fcad0e1f5451ad7a5c97b8bd
alaniswright <alan@alaniswright.com>
2022-03-23 21:23:23 +0000

    Finishing off SQL

--- Code Academy/Analyse_Data_With_SQL_Skill_Path/DataScienceIndependentProject2.sql
+++ Code Academy/Analyse_Data_With_SQL_Skill_Path/DataScienceIndependentProject2.sql
@@ -1,6 +1,8 @@
 -- Data Science Indepent Project 2
 -- https://discuss.codecademy.com/t/data-science-independent-project-2-explore-a-sample-database/419945
 
+-- Do project 3 later https://discuss.codecademy.com/t/data-science-independent-project-3-education-census-data/419947
+
 -- 1. "Which tracks appeared in the most playlists? how many playlist did they appear in?"
 -- Top 50 tracks ordered by how many playlists they appear in
 
@@ -22,7 +24,7 @@ SELECT
 	tracks.name as 'Track name',
 	SUM(invoice_items.UnitPrice) AS 'Revenue'
 FROM tracks
-JOIN invoice_items	
+JOIN invoice_items
 	ON tracks.TrackId=invoice_items.TrackId
 GROUP BY tracks.TrackId
 ORDER BY 2 DESC
@@ -42,7 +44,7 @@ GROUP BY albums.AlbumId
 ORDER BY 2 DESC
 LIMIT 50;
 
--- Top 10 genres by revenue	
+-- Top 10 genres by revenue
 
 SELECT
 	genres.name as 'Genre',
@@ -60,7 +62,7 @@ LIMIT 10;
 
 -- 50 countries with the highest sales revenue
 
-SELECT 
+SELECT
 	invoices.BillingCountry as 'Country',
 	round(sum(invoices.Total)) AS 'Revenue'
 FROM invoices
@@ -84,7 +86,7 @@ SELECT
 	employees.FirstName || ' ' || employees.LastName AS 'Employee name',
 	COUNT(customers.SupportRepId) as '# customers supported',
 	ROUND(SUM(invoices.Total) / COUNT(invoices.total), 3) as 'Average sale value',
-	ROUND(SUM(invoices.Total)) as 'Total Sale' 
+	ROUND(SUM(invoices.Total)) as 'Total Sale'
 FROM customers
 JOIN employees
 	ON customers.SupportRepId = employees.EmployeeId
@@ -93,5 +95,3 @@ JOIN invoices
 GROUP BY customers.SupportRepId
 ORDER BY 2 DESC
 LIMIT 50;
-
-
