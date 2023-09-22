c9e8464a23638b9f29eeebf38042bf8314d86781
alaniswright <alan@alaniswright.com>
2023-09-22 12:03:36 +0100

    Finsihing off the visualising data section

--- Python_For_Data_Science/5-Data Visualisation Fundamentals with Python/Lessons/Visualising categorical data.py
+++ Python_For_Data_Science/5-Data Visualisation Fundamentals with Python/Lessons/Visualising categorical data.py
@@ -20,3 +20,21 @@ sns.countplot(df["Supportive Environment"], order=value_order) # orders by value
 plt.xticks(rotation=30) # rotates the xticks
 plt.show()
 
+# Pie charts
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import codecademylib3
+
+water_usage = pd.read_csv("water_usage.csv")
+print(water_usage.head())
+
+wedge_sizes = water_usage['prop']
+pie_labels = water_usage['water_source']
+
+plt.pie(wedge_sizes, labels = pie_labels)
+plt.axis('equal')
+plt.title('Distribution of House Water Usage')
+plt.show()
+
+
